dde5a796cdafc5bd45c7a6b2f26859772092fe9f
sagarbabu <sagarsurendhrababu@gmail.com>
2026-01-06 00:17:46 +0530

    docker issue solved

--- backend/server.js
+++ backend/server.js
@@ -8,20 +8,21 @@ app.use(cors({
   origin: [
   'http://localhost:5173',
   'http://localhost:5174',
+  'http://localhost:3000',  
 ],
   credentials: true,
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
-app.use('/api/message', async (req,res) => {
+app.get('/api/message', async (req,res) => {
    try{
-     res.status(200).json("Hello from server");
+     res.status(200).json({message:"New Begnings in Kerala!"});
    }catch(err){
      res.status(500).json('Server Error');
    }
 });
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 4000, '0.0.0.0', () => {
   console.log('Server is running on port 4000');
 });
 
